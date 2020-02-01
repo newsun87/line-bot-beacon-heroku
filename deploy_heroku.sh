de49f2b18b8bed818b12c4060f9f10cc1496951d
newsun87 <newsun87@mail.sju.edu.tw>
2020-02-01 16:36:29 +0800

    init.

--- deploy_heroku.sh
+++ deploy_heroku.sh
@@ -2,6 +2,7 @@ git config --global user.email 'newsun87@mail.sju.edu.tw'
 git config --global user.name 'newsun87'
 git init
 heroku git:remote -a line-beacon-bot
+heroku config:add TZ="Asia/Taipei"
 git add .
 git commit -m "init."
 git push heroku master
